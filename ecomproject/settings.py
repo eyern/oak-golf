25c28dfe1e95359212d2ade1312cc6c6f8c4c379
eyern <eyern254@gmail.com>
2025-03-16 23:29:55 +0300

    media root persistence disk

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/var/data')
 
 
 # Default primary key field type
